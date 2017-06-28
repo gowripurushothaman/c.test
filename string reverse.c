685ca7e58eed9778b88288b064f070c17591dc3b
gowripurushothaman <gowrikalpana27@gmail.com>
2017-06-28 20:55:44 +0530

    Create string reverse.c

---
+++ string reverse.c
@@ -0,0 +1,12 @@
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char arr[20];
+printf("enter a string to reverse");
+gets(arr);
+strrev(arr);
+printf("reverse of entered string is %s\n",arr);
+reeturn 0;
+}
