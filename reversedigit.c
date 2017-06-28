3e1ca60882e9d34a4a01500cd9736de36ba97b8b
gowripurushothaman <gowrikalpana27@gmail.com>
2017-06-28 21:46:59 +0530

    Update reversedigit.c

--- reversedigit.c
+++ reversedigit.c
@@ -1,16 +1,16 @@
-#include<conio.h>
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
-int n,reverse number=0,remainder;
-printf("enter an integer");
-scanf("%d",&n);
-while(n!=0)
-{
-remainder=n%10;
-reverse number=reverse number*10+remainder;
-n=n/10;
-}
-printf("reverse number =%d",reverse number);
-return 0;
+  int n,remainder,reverse=0;
+  printf("enter the number");
+  scanf("%d",&n);
+  while(n!=0)
+  {
+    remainder=n%10;
+    reverse=reverse*10+remainder;
+    n=n/10;
+  }
+  printf("enter the reversed value %d",reverse);
+  return 0;
 }
